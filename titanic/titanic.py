6d72c00832ed82c87389ca54daa45ec7057a1685
ARTHUR PEREIRA DE GOUVEIA E SILVA <gouveia.arthur@gmail.com>
2017-02-05 15:00:50 -0200

    Preparando o dataframe train
    Preparando os dados do dataframe de treino para fazer análise
    exploratória e aplicação do modelo

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,18 +1,51 @@
 
 # coding: utf-8
 
+# # Titanic: Machine Learning from Disaster
+# ## Bibliotecas utilizadas
+
 # In[1]:
 
 import pandas as pd
 
 
+# ## Lendo o arquivo e exibindo suas extremidades
+
 # In[2]:
 
-train = pd.read_csv('datasets/train.csv')
-train.head()
+csv_train = pd.read_csv('datasets/train.csv')
+csv_train.head()
 
 
 # In[3]:
 
-train.tail()
+csv_train.tail()
+
+
+# ## Criando um novo dataframe
+# Esse novo dataframe terá apenas as colunas úteis para modelagem. Serão removidos o nome e o número do ticket, PassengerId será colocado como índice e as variáveis Sex e Embarked serão transformadas em dummy
+
+# In[12]:
+
+train = csv_train.copy()  # Cria uma cópia do dataframe
+train.set_index('PassengerId', inplace=True)  # Define PassengerId como novo índice das linhas
+dummies = pd.get_dummies(train.loc[:, ['Sex', 'Embarked']])  # transforma Sex e Embarked em variáveis dummy
+train = pd.concat([train, dummies], axis=1)  # Concatena train e dummies
+train.drop(['Sex', 'Embarked', 'Name', 'Ticket'], axis=1, inplace=True)  # Remove colunas inúteis
+train.head()
+
+
+# In[9]:
+
+dummies = pd.get_dummies(train.loc[:, ['Sex', 'Embarked']])
+
+
+# In[11]:
+
+pd.concat([train, dummies], axis=1)
+
+
+# In[8]:
+
+train.drop(['Sex', 'Embarked', 'Name', 'Ticket'], axis=1)
 
