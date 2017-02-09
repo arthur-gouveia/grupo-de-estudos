e329752cfb3bfbadafd404d4a409ac3de083313d
arthur-gouveia <gouveia.arthur@gmail.com>
2017-02-09 10:20:31 -0200

    corrigindo previsão de idade
    - Usando drop em vez de del\n - Transformando o retorno de predict em um pandas.Series para evitar warnings e tornar o código mais estável\n - Aplicando a previsão de idade diretamente em train e não em com_cabine

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -4,7 +4,7 @@
 # # Titanic: Machine Learning from Disaster
 # ## Bibliotecas utilizadas
 
-# In[29]:
+# In[1]:
 
 from numbers import Number
 
@@ -30,10 +30,15 @@ csv_train.head()
 csv_train.tail()
 
 
+# In[4]:
+
+csv_train.describe()
+
+
 # ## Criando um novo dataframe
 # Esse novo dataframe terá apenas as colunas úteis para modelagem. Serão removidos o nome e o número do ticket, PassengerId será colocado como índice e as variáveis Sex e Embarked serão transformadas em dummy
 
-# In[4]:
+# In[5]:
 
 train = csv_train.copy()  # Cria uma cópia do dataframe
 train.set_index('PassengerId', inplace=True)  # Define PassengerId como novo índice das linhas
@@ -42,10 +47,30 @@ train = pd.concat([train, dummies], axis=1)  # Concatena train e dummies
 train.drop(['Sex', 'Embarked', 'Name', 'Ticket'], axis=1, inplace=True)  # Remove colunas inúteis
 
 
+# ## Preenchendo os valores missing de idade
+
+# In[6]:
+
+X = train.loc[train.Age.notnull(), :].copy()
+Y = X.Age
+X.drop(['Cabin', 'Age'], axis=1, inplace=True)
+
+linreg = LinearRegression()
+linreg.fit(X, Y)
+
+
+X2 = train.loc[train.Age.isnull(),:].copy()
+X2.drop(['Cabin', 'Age'], axis=1, inplace=True)
+s = pd.Series(linreg.predict(X2))
+s.index = train.Age[train.Age.isnull()].index
+train.Age.fillna(value=s, inplace=True)
+train.describe()
+
+
 # # Criando a variável cabin_type
 # O número da cabine, por si só, não é uma informação muito útil. Porém a letra inicial da cabine pode ser um indicativo da localização da mesma e isso pode indicar o 'status' do passageiro que ocupa aquela cabine. Dessa forma vamos isolar a primeira letra da cabine e chamá-la de CabinType
 
-# In[5]:
+# In[7]:
 
 def gera_CabinType(cabin):
     if isinstance(cabin, Number):
@@ -66,7 +91,7 @@ train.head()
 # ## Analisando valores missing em Cabin
 # Comparando o preço e a classe dos passageiros sem número de cabine (ou com CabinType 'X')
 
-# In[6]:
+# In[8]:
 
 def gera_preços(df):
     return pd.DataFrame({'Valor mínimo': df.Fare.min(),
@@ -77,7 +102,7 @@ def gera_preços(df):
 train.groupby('Cabin_Type').apply(func=gera_preços)
 
 
-# In[7]:
+# In[9]:
 
 com_cabine = train.loc[train.Cabin.notnull(), :]
 com_cabine.boxplot('Fare', by='Cabin_Type', figsize=(12, 8))
@@ -85,7 +110,7 @@ com_cabine.boxplot('Fare', by='Cabin_Type', figsize=(12, 8))
 
 # **É possível ver que há evidências de uma relação entre CabinType e Fare. Vou testar isso com uma ANOVA**
 
-# In[8]:
+# In[10]:
 
 mod = ols('Fare ~ Cabin_Type', data=com_cabine).fit()
                 
@@ -94,27 +119,5 @@ print(aov_table)
 
 
 # Há uma diferença significativa das médias de ```Fare``` para os diversos grupos de ```CabinType```. Dessa forma é possível gerar um modelo que encontre o ```CabinType``` em função do ```Fare```
-
-# ## Preenchendo os valores missing de idade
-
-# In[46]:
-
-X = com_cabine.loc[com_cabine.Age.notnull(),:].copy()
-Y = X.Age
-del X['Cabin_Type']
-del X['Cabin']
-del X['Age']
-
-linreg = LinearRegression()
-linreg.fit(X, Y)
-
-
-X2 = com_cabine.loc[com_cabine.Age.isnull(),:].copy()
-del X2['Cabin_Type']
-del X2['Cabin']
-del X2['Age']
-com_cabine.Age[com_cabine.Age.isnull()] = linreg.predict(X2)
-com_cabine.describe()
-
 
 # ## Próximo passo: prever o tipo de cabine
